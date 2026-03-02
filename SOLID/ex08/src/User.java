99be3e7578d9066ff9fa0cb1992f017aa1cc484e
Subhan Rahiman <subhanrahiman7@gmail.com>
2026-03-02 15:43:08 +0530

    ex08: add User entity

---
+++ SOLID/ex08/src/User.java
@@ -0,0 +1,10 @@
+public class User {
+    public final String id;
+    public final String name;
+
+    public User(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
